13acff076588d2bbdf58a509196c8679bbb77220
Andrey Kolotukhin <KolotukhinAndrey@rambler.ru>
2021-12-29 13:45:49 +0300

    initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,7 @@
+public class BonusMilesService {
+    public int calculate(int cost){
+        int bonusMileCost = 20;
+        int bonusMiles = cost / bonusMileCost;
+        return bonusMiles;
+    }
+}
